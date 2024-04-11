1b30106a912d26e34bf8f7e9eafc8cc576ca557e
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-04-11 17:41:04 +0200

    aggiunta pagina di dettaglio show

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -2,22 +2,23 @@
 
 @section('content')
     <main>
-        <div class="container">
+        <div class="container mt-5">
             <div class="row row-cols-1 row-cols-md-3 g-4">
-            @foreach ($comics as $comic)
-                <div class="col">
-                  <div class="card h-100">
-                    <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}" style="max-height: 300px; object-fit: cover; object-position: top;">
-                    <div class="card-body">
-                      <h5 class="card-title">{{$comic->title}}</h5>
-                      <div class="d-flex justify-content-between">
-                        <h6 class="text-body-secondary">{{$comic->price}}</h6>
-                        <h6 class="text-body-secondary text-capitalize">{{$comic->type}}</h6>
-                      </div>
+                @foreach ($comics as $comic)
+                    <div class="col">
+                        <a class="card h-100 text-decoration-none" href="{{route('comics.show', $comic->id)}}">
+                            <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}"
+                                style="max-height: 300px; object-fit: cover; object-position: top;">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $comic->title }}</h5>
+                                <div class="d-flex justify-content-between">
+                                    <h6 class="text-body-secondary">{{ $comic->price }}</h6>
+                                    <h6 class="text-body-secondary text-capitalize">{{ $comic->type }}</h6>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                  </div>
-                </div>
-            @endforeach
+                @endforeach
 
             </div>
 
---
+++ resources/views/comic/show.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts/app')
+
+@section('content')
+<div class="container mt-5">
+    <div class="d-flex gap-5">
+        <img src="{{$comic->thumb}}" alt="{{$comic->title}}" style="max-width: 500px; object-fit: cover;" >
+        <div>
+            <h2>{{$comic->title}}</h2>
+            <div class="d-flex gap-5">
+                <h3 class="text-body-secondary">{{$comic->series}}</h3>
+                <h3 class="text-body-secondary">{{$comic->price}}</h3>
+                <h3 class="text-body-secondary">{{$comic->sale_date}}</h3>
+            </div>
+            <p>{{$comic->description}}</p>
+            <div class="d-flex gap-5">
+                <div>
+                    <h4>Arists</h4>
+                    <ul>
+                        @php
+                        foreach(json_decode($comic->artists) as $artist){
+                            echo '<li>' . $artist . '</li>';
+                        }     
+                        @endphp
+                    </ul>
+
+                </div>
+
+                <div>
+                    <h4>Writers</h4>
+                    <ul>
+                        @php
+                        foreach(json_decode($comic->writers) as $writer){
+                            echo '<li>' . $writer . '</li>';
+                        }     
+                        @endphp
+                    </ul>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
